9bdc007000a69b3779659594390d4a8732e2c4dd
Adrian Bob <bobadrian88@gmail.com>
2018-03-11 19:03:36 +0200

    - Verify if password not contains any empty characters.

--- Source/Heating/Create.cs
+++ Source/Heating/Create.cs
@@ -25,24 +25,33 @@ namespace Heating
             Animation anim = AnimationUtils.LoadAnimation(ApplicationContext, Resource.Animation.fadeIn);
             Animation anim2 = AnimationUtils.LoadAnimation(ApplicationContext, Resource.Animation.abc_fade_out);
 
-            var username = FindViewById<EditText>(Resource.Id.userNamecrt);
-            var password = FindViewById<EditText>(Resource.Id.passwordcrt);
-            var brdId = FindViewById<EditText>(Resource.Id.brdId);
+
+
             var crtBtn = FindViewById<Button>(Resource.Id.crtacc);
 
-            var client = new RestClient("http://");
+            var client = new RestClient("http://localhost:8080");
 
 
             crtBtn.Click += delegate
             {
-               
+
                 crtBtn.StartAnimation(anim);
                 crtBtn.StartAnimation(anim2);
-                
 
-                var request = new RestRequest("api/Create//");
+                var username = FindViewById<EditText>(Resource.Id.userNamecrt);
+                var password = FindViewById<EditText>(Resource.Id.passwordcrt);
+                var brdId = FindViewById<EditText>(Resource.Id.brdId);
+
+                if (StringContainsNullOrEmptyChar(password.Text))
+                {
+                    Toast.MakeText(this, "password contains empty characters", ToastLength.Long);
+                    return;
+                }
+
+                var request = new RestRequest("api/Create?user=" + username.Text + "&pass=" + password.Text + "&brdId=" + brdId.Text);
                 var result = client.Execute(request);
                 string res = result.Content;
+
 
 
                 if (res.Equals("1"))
@@ -60,7 +69,18 @@ namespace Heating
 
             };
 
+        }
+
+        private bool StringContainsNullOrEmptyChar(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return true;
+
+            var emptyCharArray = value.ToCharArray().Where(v => string.IsNullOrEmpty(v.ToString()));
+
+            return emptyCharArray.Count() <= 0;
 
         }
+
     }
 }
